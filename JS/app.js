4a58788c61df0404e6568db45e67827c887b194a
paustyna <paulina.piekielny@gmail.com>
2017-10-07 14:47:26 +0200

    konsolowe zmiany

---
+++ JS/app.js
@@ -0,0 +1,35 @@
+console.log('działa!');
+var name = "Pau";
+console.log(name);
+var fruits = ["lemon", "apple", "plum"];
+console.log(fruits);
+console.log("owoc trzeci to " + fruits[2]);
+
+
+var name1 = "Paulina";
+console.log(name1);
+
+var imiona = ["Asia","Gosia", "Paulina", "Lena", "Sonia"];
+console.log("drugie imię to "+ imiona[1]);
+
+var a = 6
+var b = 4
+
+function myFunction1() {
+    console.log("Hurra!");
+}
+
+myFunction1()
+
+if (a>b) {
+    myFunction1()
+}
+
+for (var i=0; i<=imiona.length-1; i++){
+    console.log(imiona[i]);
+}
+
+
+
+
+
