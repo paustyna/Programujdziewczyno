b5843d20cf5314d7a1c98a169c36c56587ae7df1
paustyna <paulina.piekielny@gmail.com>
2017-10-07 16:37:29 +0200

    manu chowa sie

--- JS/app.js
+++ JS/app.js
@@ -1,35 +1,46 @@
-console.log('działa!');
-var name = "Pau";
-console.log(name);
-var fruits = ["lemon", "apple", "plum"];
-console.log(fruits);
-console.log("owoc trzeci to " + fruits[2]);
+'use strict';
 
 
-var name1 = "Paulina";
-console.log(name1);
 
-var imiona = ["Asia","Gosia", "Paulina", "Lena", "Sonia"];
-console.log("drugie imię to "+ imiona[1]);
-
-var a = 6
-var b = 4
-
-function myFunction1() {
-    console.log("Hurra!");
-}
-
-myFunction1()
-
-if (a>b) {
-    myFunction1()
-}
-
-for (var i=0; i<=imiona.length-1; i++){
-    console.log(imiona[i]);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('działa');
 
 
+
+    // Zadanie 1
+
+    var forDropdown = document.querySelector('.for-dropdown');
+    console.log(forDropdown);
+
+    var dropdown = document.querySelector(".dropdown");
+    console.log(dropdown);
+
+forDropdown.addEventListener('mouseover', function() {
+    dropdown.style.display = 'block';
+    });
+
+forDropdown.addEventListener('mouseout', function() {
+    dropdown.style.display = 'none';
+});
+
+
+
+
+
+
+
+    // forDropdown.addEventListener('mouseover', function() {
+    // dropdown.style.display = 'block';
+    // });
+
+    // forDropdown.addEventListener('mouseout', function() {
+    //     dropdown.style.display = 'none';
+    // });
+       
+
+
+
+});
 
 
 
